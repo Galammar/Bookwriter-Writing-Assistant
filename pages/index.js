14ed45597fba3f4f39083d301ff357aae82cb84c
Galammar <80973810+Galammar@users.noreply.github.com>
2023-08-21 16:24:28 +0300

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -23,14 +23,17 @@ export default function Home() {
     return (
       <span>
         {'['} <span className={styles.name}>{character.name}</span> {']'}
-        <span className={styles.description}>{character.description}</span>
+        <span
+          className={styles.description}
+          dangerouslySetInnerHTML={{ __html: character.description }}
+        ></span>
       </span>
     );
   }
 
   function TableOfNames() {
     return (
-      <div>
+      <div className={styles.tableOfNames}>
         {characters.map((character, index) => (
           <Name character={character} key={index} />
         ))}
@@ -42,40 +45,63 @@ export default function Home() {
     <div>
       <Head>
         <title>Bookwriter | Writing Assistant</title>
+        <link
+          rel="icon"
+          href="https://primalfear.wiki.gg/images/f/f3/Fire_Feather.png"
+          type="image/x-icon"
+        />
       </Head>
 
-      <main>
-        <input
-          type="text"
-          id="name"
-          value={newName}
-          onChange={(e) => setNewName(e.target.value)}
-        />
-        <br />
-        <textarea
-          id="description"
-          cols="30"
-          rows="10"
-          value={newDescription}
-          onChange={(e) => setNewDescription(e.target.value)}
-        ></textarea>
-        <br />
-        <button
-          onClick={() => {
-            if (!newName == '') {
-              setCharacters(
-                characters,
-                characters.push(new Character(newName, newDescription))
-              );
-            } else {
-              alert('First enter a name for your character!');
+      <main className={styles.main}>
+        <div className={styles.leftSideBar}>
+          <div className={styles.textedDivider}>Create new character:</div>
+          <input
+            type="text"
+            id="name"
+            value={newName}
+            placeholder={'Name'}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+          <br />
+          <textarea
+            id="description"
+            className={styles.textareaDescription}
+            placeholder={
+              'Description appears when hovering over the character`s name'
             }
-            console.log(characters);
-          }}
-        >
-          {'Save >'}
-        </button>
-        <TableOfNames />
+            value={newDescription}
+            onChange={(e) => setNewDescription(e.target.value)}
+          ></textarea>
+          <br />
+          <button
+            onClick={() => {
+              if (!newName == '') {
+                setCharacters(
+                  characters,
+                  characters.push(new Character(newName, newDescription))
+                );
+              } else {
+                alert('First enter a name for your character!');
+              }
+              setNewName('');
+              setNewDescription('');
+            }}
+          >
+            {'Save >'}
+          </button>
+          <div className={styles.textedDivider}>Characters:</div>
+          <TableOfNames />
+        </div>
+
+        <div className={styles.mainPart}>
+          <textarea
+            name="editor"
+            id="editor"
+            className={styles.editor}
+          ></textarea>
+        </div>
+
+        <div className={styles.rightSideBar}></div>
       </main>
     </div>
   );
